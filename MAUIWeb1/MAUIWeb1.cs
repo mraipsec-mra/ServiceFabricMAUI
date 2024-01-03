29bc8ce944c072492e931914763b804f226b2841
MALLA RAVINDRA ADITYA <26783264+mraipsec-mra@users.noreply.github.com>
2024-01-03 16:22:04 +0530

    Initial check-in
    Initial check-in for Service Fabric + MAUI

---
+++ MAUIWeb1/MAUIWeb1.cs
@@ -0,0 +1,114 @@
+using Microsoft.ServiceFabric.Services.Communication.AspNetCore;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.Fabric;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MAUIWeb1
+{
+    /// <summary>
+    /// The FabricRuntime creates an instance of this class for each service type instance.
+    /// </summary>
+    internal sealed class MAUIWeb1 : StatelessService
+    {
+        public MAUIWeb1(StatelessServiceContext context)
+            : base(context)
+        { }
+
+        /// <summary>
+        /// Optional override to create listeners (like tcp, http) for this service instance.
+        /// </summary>
+        /// <returns>The collection of listeners.</returns>
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new ServiceInstanceListener[]
+            {
+                new ServiceInstanceListener(serviceContext =>
+                    new KestrelCommunicationListener(serviceContext, "ServiceEndpoint", (url, listener) =>
+                    {
+                        ServiceEventSource.Current.ServiceMessage(serviceContext, $"Starting Kestrel on {url}");
+
+                        var builder = MauiApp.CreateBuilder();
+                        builder
+                            .UseMauiApp<App>()
+                            .ConfigureFonts(fonts =>
+                            {
+                                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                            });
+
+                        builder.Services.AddMauiBlazorWebView();
+
+                    #if DEBUG
+    		            builder.Services.AddBlazorWebViewDeveloperTools();
+    		            builder.Logging.AddDebug();
+                    #endif
+
+
+
+
+
+
+
+                        var builder222 = WebApplication.CreateBuilder();
+
+                        builder222.Services.AddSingleton<StatelessServiceContext>(serviceContext);
+                        builder222.WebHost
+                                    .UseKestrel(opt =>
+                                    {
+                                        int port = serviceContext.CodePackageActivationContext.GetEndpoint("ServiceEndpoint").Port;
+                                        opt.Listen(IPAddress.IPv6Any, port, listenOptions =>
+                                        {
+                                            listenOptions.UseHttps(GetCertificateFromStore());
+                                        });
+                                    })
+                                    .UseContentRoot(Directory.GetCurrentDirectory())
+                                    .UseServiceFabricIntegration(listener, ServiceFabricIntegrationOptions.None)
+                                    .UseUrls(url);
+                        builder222.Services.AddRazorPages();
+                        var app = builder222.Build();
+                        if (!app.Environment.IsDevelopment())
+                        {
+                        app.UseExceptionHandler("/Error");
+                        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                        app.UseHsts();
+                        }
+                        app.UseHttpsRedirection();
+                        app.UseStaticFiles();
+                        app.UseRouting();
+                        app.UseAuthorization();
+                        app.MapRazorPages();
+                        
+                        return app;
+
+                    }))
+            };
+        }
+
+        /// <summary>
+        /// Finds the ASP .NET Core HTTPS development certificate in development environment. Update this method to use the appropriate certificate for production environment.
+        /// </summary>
+        /// <returns>Returns the ASP .NET Core HTTPS development certificate</returns>
+        private static X509Certificate2 GetCertificateFromStore()
+        {
+            string aspNetCoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (string.Equals(aspNetCoreEnvironment, "Development", StringComparison.OrdinalIgnoreCase))
+            {
+                const string aspNetHttpsOid = "1.3.6.1.4.1.311.84.1.1";
+                const string CNName = "CN=localhost";
+                using (X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+                {
+                    store.Open(OpenFlags.ReadOnly);
+                    var certCollection = store.Certificates;
+                    var currentCerts = certCollection.Find(X509FindType.FindByExtension, aspNetHttpsOid, true);
+                    currentCerts = currentCerts.Find(X509FindType.FindByIssuerDistinguishedName, CNName, true);
+                    return currentCerts.Count == 0 ? null : currentCerts[0];
+                }
+            }
+            else
+            {
+                throw new NotImplementedException("GetCertificateFromStore should be updated to retrieve the certificate for non Development environment");
+            }
+        }
+    }
+}
---
+++ MAUIWeb1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUIWeb1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
